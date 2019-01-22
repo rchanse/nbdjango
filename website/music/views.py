3d31ddf59bb4d4678f8d5504adc5dc209378d771
R. C. Hansen <rchansen@gmail.com>
2019-01-21 21:53:25 -0600

    modified:   rchnotes 	modified:   website/music/views.py Build a more flexible set of views

--- website/music/views.py
+++ website/music/views.py
@@ -1,20 +1,17 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>This will be a list of all albumsb</h1>")
+    all_albums = Album.objects.all()     # connect to DB get data
+    html = ''
+    for album in all_albums:             # access albums one-by-one
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'   
+    return HttpResponse(html)
+#   return HttpResponse("<h1>This will be a list of all albumsb</h1>")
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for album_id:  " + str(album_id) + " </h2>")
-
-# -*- coding: utf-8 -*-
-#
-#from django.shortcuts import render
-#from . import views
-#
-## Create your views here.
-#
-#urlpatterns = [
-#        url(r'^$' , views.index, name='index'),
-#        ]
 
