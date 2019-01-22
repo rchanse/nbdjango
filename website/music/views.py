0ed061ef8a9c3b1b6910a52df6465feb2c3eb1dc
R. C. Hansen <rchansen@gmail.com>
2019-01-21 22:53:32 -0600

    modified:   rchnotes 	new file:   website/music/templates/music/index.html 	modified:   website/music/views.py Introduce templates,  give example of template dir structure and show how to move  html info to templates

--- website/music/views.py
+++ website/music/views.py
@@ -1,16 +1,26 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.template import loader             # tut 14
 from .models import Album
-
 
 def index(request):
     all_albums = Album.objects.all()     # connect to DB get data
-    html = ''
-    for album in all_albums:             # access albums one-by-one
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'   
-    return HttpResponse(html)
-#   return HttpResponse("<h1>This will be a list of all albumsb</h1>")
+    template =  loader.get_template('music/index.html')
+    # pass var by a dict   std is call dix  context
+    context = {
+            'all_albums': all_albums,
+            }
+    return HttpResponse(template.render(context, request))
+# material below was in prev tut 12
+##def index(request):
+##    all_albums = Album.objects.all()     # connect to DB get data
+##    html = ''
+##
+##for album in all_albums:             # access albums one-by-one
+##        url = '/music/' + str(album.id) + '/'
+##        html += '<a href="' + url + '">' + album.album_title + '</a><br>'   
+##    return HttpResponse(html)
+###   return HttpResponse("<h1>This will be a list of all albumsb</h1>")
 
 def detail(request,album_id):
     return HttpResponse("<h2>Details for album_id:  " + str(album_id) + " </h2>")
