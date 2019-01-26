0b3a277dd07464d32ba46d06f799965d3b7a1fe8
R. C. Hansen <rchansen@gmail.com>
2019-01-26 15:03:27 -0600

    Changes to be committed:  	modified:   rchnotes  	modified:   website/db.sqlite3  	modified:   website/music/admin.py  	modified:   website/music/models.py Add a song to db  1  modified model and admin  2 sudo python manage.py shell

--- website/music/admin.py
+++ website/music/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Album
+from .models import Album, Song
 
 admin.site.register(Album)
+admin.site.register(Song)
