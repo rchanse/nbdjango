08e1ced1a82215ced62bad476a9c8f9ccfdbef37
R. C. Hansen <rchansen@gmail.com>
2019-01-26 17:39:10 -0600

    Changes to be committed:  	modified:   rchnotes  	modified:   website/music/templates/music/index.html  	new file:   website/music/templates/music/index.pretut20.html  	modified:   website/music/urls.py show how to remove some hardcoded info in url addresses by referring to the name= field in  associated.urls.py

--- website/music/urls.py
+++ website/music/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 #    url(r'^admin/$  ', admin.site.urls),
-    # /music/712/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detai'),
+    # /music/712/   /music/album_id/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
